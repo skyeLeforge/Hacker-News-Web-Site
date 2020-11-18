1c1a03e9cd5fae2c142b606f8d900dd44cb6f159
Skye <skyeleforge@gmail.com>
2020-11-18 14:10:36 -0500

    init commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {useEffect, useState} from 'react';
+import {StoriesContainer} from './containers/StoriesContainer';
+
+export const App = () => {
+
+
+  return (
+    <StoriesContainer />
+  );
+
+};
+
+
